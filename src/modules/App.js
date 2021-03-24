14035ee3c3ad9610c8729219ef2d1344cdb94b5e
hakkonen <rnlds.j@gmail.com>
2021-03-24 19:54:20 +1100

    v0
    Boilerpalte

--- src/modules/App.js
+++ src/modules/App.js
@@ -1,22 +1,10 @@
 import elFactory from "../functions/elementFactory"
 
-import * as links from "./Links"
-import { cube } from "./Cube"
-import * as pageOne from "./PageOne/PageOne"
-import * as pageTwo from "./Pages/PageTwo"
-
 const App = () => {
     
 
     return ([
-        // Links
-        links.folio(),
-        links.info(),
-        links.mode(),
-        links.social(),
-
-        // Combined cube faces
-        cube()
+        
     ])
 }
 
