a55a4ac6b8d74e62b897a8b70859f332dead7989
hakkonen <rnlds.j@gmail.com>
2021-03-24 14:06:01 +1100

    v0.3
    Created first folio parallax page

--- src/modules/App.js
+++ src/modules/App.js
@@ -31,15 +31,21 @@ const App = () => {
     const parallaxGroupOne = elFactory("div", {id: "group-one", class: "parallax__group"})
 
     // Name header
-    const parallaxOne = elFactory("div", {class: "parallax__layer parallax__layer--base"},
-        elFactory("div", {class: "header"},
+    const parallaxOne = elFactory("div", {class: "parallax__layer parallax__layer--base parallax--one"},
+        elFactory("div", {id: "header",class: "header"},
             elFactory("span", {}, "JAYDEN"),
             elFactory("br", {}),
             elFactory("span", {}, "REYNOLDS"),
         )
     )
 
-    // const parallaxOneSubHeader = elFactory("div", {class: "parallax__layer parallax__layer--six folio__sub-header"}, "WEB DEV")
+    // Header animation
+    window.onload = () => {
+        const header = document.getElementById("header")
+        header.style.opacity = "1"
+        header.style.filter = "blur(0)"
+        console.log("loaded")
+    }
 
     parallaxGroupOne.appendChild(parallaxOne)
 
@@ -47,18 +53,40 @@ const App = () => {
     const parallaxGroupTwo = elFactory("div", {id: "group-two",class: "parallax__group"})
 
     // P2 page -- folio
-    const parallaxTwo = elFactory("div", {class: "parallax__layer parallax__layer--base"},
-        elFactory("div", {class: "title"},
-            elFactory("div", {class: "folio__header"}, "FOLIO")
+    const parallaxTwo = elFactory("div", {class: "parallax__layer parallax__layer--base parallax--two"},
+        elFactory("div", {class: "parallax--two__header"}, "FOLIO")
+    )
+
+    const parallaxTwoSubHeader = elFactory("div", {class: "parallax__layer parallax__layer--fore parallax--two__subheader"}, "WEB DEV")
+
+    const parallaxThree = elFactory("div", {class: "parallax__layer parallax__layer--back parallax--three"}, 
+        elFactory('div', {class: "title"})
+    )
+
+    const parallaxGroupTwoTile__one = elFactory("div", {class: "parallax__layer parallax__layer--fore--tile1 parallax__tiles--one"}, 
+        elFactory("div", {class: "tile"}, 
+            elFactory("img", {src: web1}),
+        ),
+    )
+
+    const parallaxGroupTwoTile__two = elFactory("div", {class: "parallax__layer parallax__layer--fore--tile2 parallax__tiles--two"},
+        elFactory("div", {class: "tile"}, 
+            elFactory("img", {src: web3}),
         )
     )
 
-    const parallaxThree = elFactory("div", {class: "parallax__layer parallax__layer--back bg-dark-green"}, 
-        elFactory('div', {class: "title"}, "layer four")
+    const parallaxGroupTwoTile__three = elFactory("div", {class: "parallax__layer parallax__layer--fore--tile3 parallax__tiles--three"},
+        elFactory("div", {class: "tile"}, 
+            elFactory("img", {src: web2}),
+        ),
     )
 
     parallaxGroupTwo.appendChild(parallaxTwo)
+    parallaxGroupTwo.appendChild(parallaxTwoSubHeader)
     parallaxGroupTwo.appendChild(parallaxThree)
+    parallaxGroupTwo.appendChild(parallaxGroupTwoTile__one)
+    parallaxGroupTwo.appendChild(parallaxGroupTwoTile__two)
+    parallaxGroupTwo.appendChild(parallaxGroupTwoTile__three)
 
     // GROUP THREE
 
@@ -74,16 +102,25 @@ const App = () => {
 
     const parallaxGroupFour = elFactory("div", {id: "group-four", class: "parallax__group"})
 
-    const parallaxFive = elFactory("div", {class: "parallax__layer parallax__layer--deep bg-light"})
+    const parallaxFive = elFactory("div", {class: "parallax__layer parallax__layer--deep parallax--five"}, "deep bg")
 
     parallaxGroupFour.appendChild(parallaxFive)
+
+    // GROUP FIVE
+
+    const parallaxGroupFive = elFactory("div", {id: "group-five", class: "parallax__group"})
+
+    const parallaxSix = elFactory("div", {class: "parallax__layer parallax__layer--base parallax--six"}, "layer six")
+
+    parallaxGroupFive.appendChild(parallaxSix)
 
     return ([
         mode,
         parallaxGroupOne,
         parallaxGroupTwo,
         parallaxGroupThree,
-        parallaxGroupFour
+        parallaxGroupFour,
+        parallaxGroupFive
     ])
 }
 
