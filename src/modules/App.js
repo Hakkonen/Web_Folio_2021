85899343049d542268e4fd25ee15d5f7e9186e65
hakkonen <rnlds.j@gmail.com>
2021-03-24 17:38:11 +1100

    v0.3
    Reformat visual layout

--- src/modules/App.js
+++ src/modules/App.js
@@ -2,16 +2,40 @@ import elFactory from "../functions/elementFactory"
 import web1 from "../images/web1.png"
 import web2 from "../images/web2.png"
 import web3 from "../images/web3.png"
+import sam1 from "../images/sam1.jpg"
 
 const App = () => {
 
-    // Backplate
-    const basePage = elFactory("div", {class: "base-page"},
-        // elFactory("div", {class: "header"}, "Jayden Reynolds")
+    // Splash layer & header card
+    const splash = elFactory("div", {id: "splash", class: "splash"},
+        elFactory("div", {id: "header", class: "header"},
+            elFactory("span", {}, "JAYDEN"),
+            elFactory("br", {}),
+            elFactory("span", {}, "REYNOLDS"),
+        )
     )
 
+    // Header animation
+    window.onload = () => {
+        const header = document.getElementById("header")
+        header.style.opacity = "1"
+        header.style.filter = "blur(0)"
+        console.log("loaded")
+        // Fade in and out header card
+        setTimeout(() => {
+            const getSplash = document.getElementById("splash")
+            getSplash.style.opacity = 0;
+            getSplash.style.filter = "blur(1.5rem)"
+            // Removes div after animation
+            setTimeout(() => {
+                document.getElementById("root").removeChild(getSplash)
+            }, [2000])
+            // add zero back in
+        }, [300])
+    }
+
     // Night mode
-    const mode = elFactory("span", {class: "mode no-select"}, "night")
+    const mode = elFactory("span", {class: "mode no-select"}, "debug")
     mode.addEventListener("click", () => {
         // const root = document.getElementById("root")
         // if(root.classList.contains("theme--light")) {
@@ -30,23 +54,26 @@ const App = () => {
     // GROUP ONE
     const parallaxGroupOne = elFactory("div", {id: "group-one", class: "parallax__group"})
 
-    // Name header
-    const parallaxOne = elFactory("div", {class: "parallax__layer parallax__layer--base parallax--one"},
-        elFactory("div", {id: "header",class: "header"},
-            elFactory("span", {}, "JAYDEN"),
+    const parallaxOne = elFactory("div", {class: "parallax__layer parallax__layer--base parallax--one"})
+
+    const heroImage = elFactory("div", {class: "hero-image"},
+        elFactory("img", {src: sam1})
+    )
+
+    const heroHeader = elFactory("div", {class: "parallax__layer parallax__layer--fore parallax--header"},
+        elFactory("span", {class: "hero-header"},
+            elFactory("span", {}, "WEB"),
             elFactory("br", {}),
-            elFactory("span", {}, "REYNOLDS"),
+            elFactory("span", {}, "DEV &"),
+            elFactory("br", {}),
+            elFactory("span", {}, "PHOTO"),
+            elFactory("br", {}),
+            elFactory("span", {}, "GRAPHY"),
         )
     )
 
-    // Header animation
-    window.onload = () => {
-        const header = document.getElementById("header")
-        header.style.opacity = "1"
-        header.style.filter = "blur(0)"
-        console.log("loaded")
-    }
-
+    parallaxOne.appendChild(heroImage)
+    parallaxGroupOne.appendChild(heroHeader)
     parallaxGroupOne.appendChild(parallaxOne)
 
     // GROUP TWO
@@ -54,73 +81,52 @@ const App = () => {
 
     // P2 page -- folio
     const parallaxTwo = elFactory("div", {class: "parallax__layer parallax__layer--base parallax--two"},
-        elFactory("div", {class: "parallax--two__header"}, "FOLIO")
+        elFactory("div", {class: "parallax--two__header"},
+            elFactory("div", {class: "title"}, "layer two")
+        )
     )
-
-    const parallaxTwoSubHeader = elFactory("div", {class: "parallax__layer parallax__layer--fore parallax--two__subheader"}, "WEB DEV")
 
     const parallaxThree = elFactory("div", {class: "parallax__layer parallax__layer--back parallax--three"}, 
         elFactory('div', {class: "title"})
     )
 
-    const parallaxGroupTwoTile__one = elFactory("div", {class: "parallax__layer parallax__layer--fore--tile1 parallax__tiles--one"}, 
-        elFactory("div", {class: "tile"}, 
-            elFactory("img", {src: web1}),
-        ),
-    )
-
-    const parallaxGroupTwoTile__two = elFactory("div", {class: "parallax__layer parallax__layer--fore--tile2 parallax__tiles--two"},
-        elFactory("div", {class: "tile"}, 
-            elFactory("img", {src: web3}),
-        )
-    )
-
-    const parallaxGroupTwoTile__three = elFactory("div", {class: "parallax__layer parallax__layer--fore--tile3 parallax__tiles--three"},
-        elFactory("div", {class: "tile"}, 
-            elFactory("img", {src: web2}),
-        ),
-    )
-
     parallaxGroupTwo.appendChild(parallaxTwo)
-    parallaxGroupTwo.appendChild(parallaxTwoSubHeader)
     parallaxGroupTwo.appendChild(parallaxThree)
-    parallaxGroupTwo.appendChild(parallaxGroupTwoTile__one)
-    parallaxGroupTwo.appendChild(parallaxGroupTwoTile__two)
-    parallaxGroupTwo.appendChild(parallaxGroupTwoTile__three)
 
     // GROUP THREE
 
     const parallaxGroupThree = elFactory("div", {id: "group-three", class: "parallax__group"})
 
-    const parallaxFour = elFactory("div", {class: "parallax__layer parallax__layer--base"}, 
+    const parallaxFour = elFactory("div", {class: "parallax__layer parallax__layer--base parallax--four"}, 
         elFactory('div', {class: "title"}, "layer four")
     )
 
     parallaxGroupThree.appendChild(parallaxFour)
 
-    // GROUP FOUR
+    // // GROUP FOUR
 
-    const parallaxGroupFour = elFactory("div", {id: "group-four", class: "parallax__group"})
+    // const parallaxGroupFour = elFactory("div", {id: "group-four", class: "parallax__group"})
 
-    const parallaxFive = elFactory("div", {class: "parallax__layer parallax__layer--deep parallax--five"}, "deep bg")
+    // const parallaxFive = elFactory("div", {class: "parallax__layer parallax__layer--deep parallax--five"}, "deep bg")
 
-    parallaxGroupFour.appendChild(parallaxFive)
+    // parallaxGroupFour.appendChild(parallaxFive)
 
-    // GROUP FIVE
+    // // GROUP FIVE
 
-    const parallaxGroupFive = elFactory("div", {id: "group-five", class: "parallax__group"})
+    // const parallaxGroupFive = elFactory("div", {id: "group-five", class: "parallax__group"})
 
-    const parallaxSix = elFactory("div", {class: "parallax__layer parallax__layer--base parallax--six"}, "layer six")
+    // const parallaxSix = elFactory("div", {class: "parallax__layer parallax__layer--base parallax--six"}, "layer six")
 
-    parallaxGroupFive.appendChild(parallaxSix)
+    // parallaxGroupFive.appendChild(parallaxSix)
 
     return ([
+        splash,
         mode,
         parallaxGroupOne,
         parallaxGroupTwo,
         parallaxGroupThree,
-        parallaxGroupFour,
-        parallaxGroupFive
+        // parallaxGroupFour,
+        // parallaxGroupFive
     ])
 }
 
